57e0f688d9e7f4639252d6ef118c37a773416ebf
Sergey Pomadin <s.pomadin@2gis.ru>
2014-12-15 16:47:09 +0600

    [ERM-5415] added the projects

---
+++ NuClear.AdvancedSearch.EntityDataModel.OData.Tests/EdmModelBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using NuClear.AdvancedSearch.EntityDataModel.OData.Building;
+
+using NUnit.Framework;
+
+namespace NuClear.AdvancedSearch.EntityDataModel.OData.Tests
+{
+    [TestFixture]
+    internal class EdmModelBuilderTests
+    {
+        private EdmModelBuilder _modelBuilder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _modelBuilder = new EdmModelBuilder();
+        }
+
+        [Test]
+        public void ShouldBuildModel()
+        {
+            var model = _modelBuilder.Build();
+            Assert.IsNotNull(model);
+        }
+    }
+}
---
+++ NuClear.AdvancedSearch.EntityDataModel.OData/Building/EdmModelBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Library;
+
+namespace NuClear.AdvancedSearch.EntityDataModel.OData.Building
+{
+    public sealed class EdmModelBuilder
+    {
+        public IEdmModel Build()
+        {
+            return new EdmModel();
+        }
+    }
+}
