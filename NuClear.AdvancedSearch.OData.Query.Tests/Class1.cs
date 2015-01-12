ae29270d1fac80191be2d33f3c3a2e7290619994
m.pashuk <m.pashuk@2gis.ru>
2015-01-12 13:32:34 +0600

    добавил проект для тестов

---
+++ NuClear.AdvancedSearch.OData.Query.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuClear.AdvancedSearch.OData.Query.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ NuClear.AdvancedSearch.OData.Query/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.OData;
+using System.Web.OData.Query;
+using System.Web.OData.Routing;
+
+using Microsoft.OData.Core.UriParser;
+using Microsoft.OData.Edm;
+
+namespace NuClear.AdvancedSearch.OData.Query
+{
+    public sealed class Class1
+    {
+        private IEdmModel
+
+        public void Method1()
+        {
+            var model = (IEdmModel)null;
+            var elementClrType = (Type)null;
+
+            var context = new ODataQueryContext(model, elementClrType, null);
+
+            var queryOptions = new Dictionary<string, string>
+            {
+                {"$skip", "1"}
+            };
+            var queryOptionParser = new ODataQueryOptionParser(context.Model, context.ElementType, context.NavigationSource, queryOptions);
+            var filterQueryOption = new FilterQueryOption(null, context, queryOptionParser);
+
+            filterQueryOption.ApplyTo();
+        }
+    }
+}
