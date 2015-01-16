22a3784a5ff2ab0ba3cc2b2682749b44e937d5af
m.pashuk <m.pashuk@2gis.ru>
2015-01-16 18:35:14 +0600

    минимальный composition root

---
+++ NuClear.AdvancedSearch.Web.OData/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace NuClear.AdvancedSearch.Web.OData.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Get(int id)
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ NuClear.AdvancedSearch.Web.OData/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using NuClear.AdvancedSearch.Web.OData;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace NuClear.AdvancedSearch.Web.OData
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
