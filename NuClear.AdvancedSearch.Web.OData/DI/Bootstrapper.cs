40eb358e82f3a8b5a9259482e56aa020e35fd3a5
m.pashuk <m.pashuk@2gis.ru>
2015-01-22 14:06:38 +0600

    simple DI

---
+++ NuClear.AdvancedSearch.Web.OData/DI/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace NuClear.AdvancedSearch.Web.OData.DI
+{
+    internal static class Bootstrapper
+    {
+        public static IUnityContainer ConfigureUnity()
+        {
+            var container = new UnityContainer();
+
+            return container;
+        }
+    }
+}
--- NuClear.AdvancedSearch.Web.OData/Startup.cs
+++ NuClear.AdvancedSearch.Web.OData/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Http;
+
 using Microsoft.Owin;
 using NuClear.AdvancedSearch.Web.OData;
+using NuClear.AdvancedSearch.Web.OData.DI;
+
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -12,6 +15,10 @@ namespace NuClear.AdvancedSearch.Web.OData
         public void Configuration(IAppBuilder appBuilder)
         {
             var config = new HttpConfiguration();
+
+            // DI
+            var container = Bootstrapper.ConfigureUnity();
+            config.DependencyResolver = new UnityResolver(container);
 
             config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
