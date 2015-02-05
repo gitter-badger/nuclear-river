aec92c275190e1df18a5f8eb407799c1ed956cbf
m.pashuk <m.pashuk@2gis.ru>
2015-02-05 15:45:44 +0600

    view for contacts

---
+++ TestData/4.Contact.sql
@@ -0,0 +1,13 @@
+﻿create view CustomerIntelligence.Contact
+as
+with
+
+ContactActive as (select * from Billing.Contacts where IsActive = 1 and IsDeleted = 0)
+
+select
+Id,
+AccountRole Role,
+IsFired,
+ClientId
+
+from ContactActive C
