3ca95a167dc384287da8415fd18e33a5614bce03
m.pashuk <m.pashuk@2gis.ru>
2015-02-06 17:18:40 +0600

    work with 2 different IEdmModel

--- Web.OData/DataAccess/ODataDbContext.cs
+++ Web.OData/DataAccess/ODataDbContext.cs
@@ -8,6 +8,12 @@ namespace NuClear.AdvancedSearch.Web.OData.DataAccess
 {
     public sealed class ODataDbContext : DbContext
     {
+        static ODataDbContext()
+        {
+            // disable DbContext type cache
+            Database.SetInitializer(new NullDatabaseInitializer<ODataDbContext>());
+        }
+
         public ODataDbContext(IEdmModel model, ODataConnectionFactory connectionFactory)
             : base(connectionFactory.CreateConnection(model.GetMetadataIdentity()), model.GetDbCompiledModel(), true)
         {
