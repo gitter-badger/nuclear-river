027e918c0fac411da700e8ddf0d0aed0636397de
m.pashuk <m.pashuk@2gis.ru>
2015-02-17 11:22:19 +0600

    buildtools

---
+++ build/default.ps1
@@ -0,0 +1,77 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+if (Test-Path 'Env:\TEAMCITY_VERSION') {
+	FormatTaskName "##teamcity[progressMessage '{0}']"
+}
+
+Import-Module "$BuildToolsRoot\modules\msbuild.psm1" -DisableNameChecking
+Import-Module "$BuildToolsRoot\modules\nuget.psm1" -DisableNameChecking
+Import-Module "$BuildToolsRoot\modules\versioning.psm1" -DisableNameChecking
+Import-Module "$BuildToolsRoot\modules\unittests.psm1" -DisableNameChecking
+
+Task Default -depends Hello
+Task Hello { "Билдскрипт запущен без цели, укажите цель" }
+
+Task Set-BuildNumber {
+	$version = Get-Version
+	
+	if (Test-Path 'Env:\TEAMCITY_VERSION') {
+		Write-Host "##teamcity[buildNumber '$($version.SemanticVersion)']"
+	}
+}
+
+Task Update-AssemblyInfo {
+	$globalDir = Join-Path $global:Context.Dir.Solution '.'
+	$assemblyInfos = Get-ChildItem $globalDir -Filter 'AssemblyInfo.cs' -Recurse
+	Update-AssemblyInfo $assemblyInfos
+}
+
+Task Build-NuGet -depends Set-BuildNumber, Update-AssemblyInfo {
+
+	$SolutionRelatedAllProjectsDir = '.'
+
+	$tempDir = Join-Path $global:Context.Dir.Temp 'NuGet'
+	if (!(Test-Path $tempDir)){
+		md $tempDir | Out-Null
+	}
+
+	Create-NuspecFiles $SolutionRelatedAllProjectsDir
+
+	$projects = Find-Projects $SolutionRelatedAllProjectsDir -Exclude '*.Tests.csproj'
+	
+	# bug (https://nuget.codeplex.com/workitem/4013) NuGet fails to extract properties from PCL project
+	# bug will be fixed in NuGet 3.0
+	$Properties = @{
+		'Version' = (Get-Version).SemanticVersion
+		'Author' = '2GIS'
+		'Description' = 'This is Release version of assembly'
+	}
+	
+	Build-PackagesFromProjects $projects $tempDir $Properties
+	
+	Publish-Artifacts $tempDir 'NuGet'
+}
+
+Task Deploy-NuGet {
+	$artifactName = Get-Artifacts 'NuGet'
+
+	$source = 'http://nuget.2gis.local'
+	$apiKey = ':enrbq rjl'
+
+	$packges = Get-ChildItem $artifactName -Include '*.nupkg' -Exclude '*.symbols.nupkg' -Recurse
+	Deploy-Packages $packges $source $apiKey
+}
+
+Task Run-UnitTests -depends Set-BuildNumber, Update-AssemblyInfo{
+	$SolutionRelatedAllProjectsDir = '.'
+	
+	$projects = Find-Projects $SolutionRelatedAllProjectsDir '*Tests*'
+	foreach($project in $Projects){
+		$buildFileName = Create-BuildFile $project.FullName
+		Invoke-MSBuild $buildFileName
+	}
+
+	Run-UnitTests $projects
+}
---
+++ build/run-build.ps1
@@ -0,0 +1,43 @@
+﻿param([string[]]$TaskList = @(), [hashtable]$Properties = @{})
+
+if ($TaskList.Count -eq 0){
+	$TaskList = @('Run-UnitTests')
+}
+
+if ($Properties.Count -eq 0){
+ 	$Properties = @{
+		'Revision' = '000000'
+		'Build' = 0
+		'Branch' = 'local'
+	}
+}
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+cls
+
+$Properties.GlobalVersion = '0.0.0'
+
+$Properties.BuildFile = Join-Path $PSScriptRoot 'default.ps1'
+$Properties.Dir = @{
+	'Solution' = Join-Path $PSScriptRoot '..'
+	'Temp' = Join-Path $PSScriptRoot 'temp'
+	'Artifacts' = Join-Path $PSScriptRoot 'artifacts'
+}
+
+# Restore-Packages
+& {
+	$NugetPath = Join-Path $Properties.Dir.Solution '.nuget\NuGet.exe'
+	if (!(Test-Path $NugetPath)){
+		$webClient = New-Object System.Net.WebClient
+		$webClient.UseDefaultCredentials = $true
+		$webClient.Proxy.Credentials = $webClient.Credentials
+		$webClient.DownloadFile('https://www.nuget.org/nuget.exe', $NugetPath)
+	}
+	$solution = Get-ChildItem $Properties.Dir.Solution -Filter '*.sln'
+	& $NugetPath @('restore', $solution.FullName, '-NonInteractive', '-Verbosity', 'quiet')
+}
+
+Import-Module "$($Properties.Dir.Solution)\packages\2GIS.NuClear.BuildTools.0.0.10\tools\buildtools.psm1" -DisableNameChecking
+Run-Build $TaskList $Properties
