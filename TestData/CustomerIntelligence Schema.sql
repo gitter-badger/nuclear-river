6799616a9621f8a53840548c68ba3038d71f1af5
Sergey Pomadin <s.pomadin@2gis.ru>
2015-03-16 16:45:16 +0600

    [ERM-5990]: updated schemas

---
+++ TestData/CustomerIntelligence Schema.sql
@@ -0,0 +1,77 @@
+-- create schema
+if not exists (select * from sys.schemas where name = 'CustomerIntelligence')
+	exec('create schema CustomerIntelligence')
+go
+
+-- drop tables
+if object_id('CustomerIntelligence.FirmCategoryGroups') is not null drop table CustomerIntelligence.FirmCategoryGroups;
+if object_id('CustomerIntelligence.FirmCategories') is not null drop table CustomerIntelligence.FirmCategories;
+if object_id('CustomerIntelligence.FirmAccount') is not null drop table CustomerIntelligence.FirmAccount;
+if object_id('CustomerIntelligence.Firm') is not null drop table CustomerIntelligence.Firm;
+
+if object_id('CustomerIntelligence.Contact') is not null drop table CustomerIntelligence.Contact;
+if object_id('CustomerIntelligence.Client') is not null drop table CustomerIntelligence.Client;
+go
+
+
+-- Client
+create table CustomerIntelligence.Client(
+	Id bigint not null
+    , Name nvarchar(250) not null
+    , CategoryGroupId bigint not null
+    , constraint PK_Clients primary key (Id)
+)
+go
+
+-- Contact
+create table CustomerIntelligence.Contact(
+	Id bigint not null
+	, [Role] int not null
+	, IsFired bit not null constraint DF_Contacts_IsFired default 0
+    , ClientId bigint not null
+    , constraint PK_Contacts primary key (Id)
+)
+go
+
+-- Firm
+create table CustomerIntelligence.Firm(
+	Id bigint not null
+    , Name nvarchar(250) not null
+    , CreatedOn datetime2(2) not null
+    , LastDisqualifiedOn datetime2(2) not null
+    , LastDistributedOn datetime2(2) not null
+    , HasPhone bit not null constraint DF_Firms_HasPhone default 0
+    , HasWebsite bit not null constraint DF_Firms_HasWebsite default 0
+    , AddressCount int not null constraint DF_Firms_AddressCount default 0
+    , CategoryGroupId bigint not null
+    , ClientId bigint null
+    , OrganizationUnitId bigint not null
+    , TerritoryId bigint not null
+    , constraint PK_Firms primary key (Id)
+)
+go
+
+-- FirmAccount
+create table CustomerIntelligence.FirmAccount(
+	AccountId bigint not null
+    , FirmId bigint not null
+    , Balance decimal(19,4) not null
+    , constraint PK_FirmAccounts primary key (AccountId, FirmId)
+)
+go
+
+-- FirmCategories
+create table CustomerIntelligence.FirmCategories(
+	FirmId bigint not null
+    , CategoryId bigint not null
+    , constraint PK_FirmCategories primary key (FirmId, CategoryId)
+)
+go
+
+-- FirmCategoryGroups
+create table CustomerIntelligence.FirmCategoryGroups(
+	FirmId bigint not null
+    , CategoryGroupId bigint not null
+    , constraint PK_FirmCategoryGroups primary key (FirmId, CategoryGroupId)
+)  
+go
---
+++ TestData/ERM Facts Schema.sql
@@ -0,0 +1,83 @@
+-- create schema
+if not exists (select * from sys.schemas where name = 'ERM')
+	exec('create schema ERM')
+go
+
+-- drop tables
+if object_id('ERM.FirmCategoryGroups') is not null drop table ERM.FirmCategoryGroups;
+if object_id('ERM.FirmCategories') is not null drop table ERM.FirmCategories;
+if object_id('ERM.FirmAccount') is not null drop table ERM.FirmAccount;
+if object_id('ERM.Firm') is not null drop table ERM.Firm;
+
+if object_id('ERM.Contact') is not null drop table ERM.Contact;
+if object_id('ERM.Client') is not null drop table ERM.Client;
+go
+
+
+-- Client
+create table ERM.Client(
+	Id bigint not null
+    , Name nvarchar(250) not null
+    , HasPhone bit not null constraint DF_Clients_HasPhone default 0
+    , HasWebsite bit not null constraint DF_Clients_HasWebsite default 0
+    , constraint PK_Clients primary key (Id)
+)
+go
+
+-- Contact
+create table ERM.Contact(
+	Id bigint not null
+	, [Role] int not null
+	, IsFired bit not null constraint DF_Contacts_IsFired default 0
+    , HasPhone bit not null constraint DF_Contacts_HasPhone default 0
+    , HasWebsite bit not null constraint DF_Contacts_HasWebsite default 0
+    , ClientId bigint not null
+    , constraint PK_Contacts primary key (Id)
+    --, constraint FK_Contacts_Clients foreign key (ClientId) references ERM.Clients (Id)
+)
+go
+
+-- Firm
+create table ERM.Firm(
+	Id bigint not null
+    , Name nvarchar(250) not null
+    , CreatedOn datetime2(2) not null
+    , LastDisqualifiedOn datetime2(2) not null
+    , LastDistributedOn datetime2(2) not null
+    , HasPhone bit not null constraint DF_Firms_HasPhone default 0
+    , HasWebsite bit not null constraint DF_Firms_HasWebsite default 0
+    , AddressCount int not null constraint DF_Firms_AddressCount default 0
+    , ClientId bigint null
+    , OrganizationUnitId bigint not null
+    , TerritoryId bigint not null
+    , constraint PK_Firms primary key (Id)
+    --, constraint FK_Firms_Clients foreign key (ClientId) references ERM.Clients (Id)
+)
+go
+
+-- FirmAccount
+create table ERM.FirmAccount(
+	AccountId bigint not null
+    , FirmId bigint not null
+    , Balance decimal(19,4) not null
+    , constraint PK_FirmAccounts primary key (AccountId, FirmId)
+)
+go
+
+-- FirmCategories
+create table ERM.FirmCategories(
+	FirmId bigint not null
+    , CategoryId bigint not null
+    , constraint PK_FirmCategories primary key (FirmId, CategoryId)
+    --, constraint FK_FirmCategories_Firms foreign key (FirmId) references ERM.Firms (Id)
+)
+go
+
+-- FirmCategoryGroups
+create table ERM.FirmCategoryGroups(
+	FirmId bigint not null
+    , CategoryGroupId bigint not null
+    , constraint PK_FirmCategoryGroups primary key (FirmId, CategoryGroupId)
+    --, constraint FK_FirmCategoryGroups_Firms foreign key (FirmId) references ERM.Firms (Id)
+)
+go
