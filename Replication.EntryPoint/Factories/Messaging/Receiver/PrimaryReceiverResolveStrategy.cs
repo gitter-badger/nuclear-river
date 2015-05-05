a94f02a936f93630d01c566eea647dc5dc118360
a.rechkalov <a.rechkalov@2gis.ru>
2015-05-05 15:40:11 +0600

    Вернул работу с настоящим ServiceBus

--- Replication.EntryPoint/Factories/Messaging/Receiver/PrimaryReceiverResolveStrategy.cs
+++ Replication.EntryPoint/Factories/Messaging/Receiver/PrimaryReceiverResolveStrategy.cs
@@ -1,15 +1,14 @@
 ﻿using NuClear.Messaging.DI.Factories.Unity.Receivers.Resolvers;
 using NuClear.Metamodeling.Provider;
 using NuClear.OperationsProcessing.API.Metadata;
-using NuClear.Replication.OperationsProcessing.Transports;
-using NuClear.Replication.OperationsProcessing.Transports.InProc;
+using NuClear.OperationsProcessing.Transports.ServiceBus.Primary;
 
 namespace NuClear.AdvancedSearch.Replication.EntryPoint.Factories.Messaging.Receiver
 {
     public sealed class PrimaryReceiverResolveStrategy : MessageFlowReceiverResolveStrategyBase
     {
         public PrimaryReceiverResolveStrategy(IMetadataProvider metadataProvider)
-            : base(metadataProvider, typeof(ServiceBusImitationReceiver), PerformedOperations.IsPerformedOperationsPrimarySource)
+            : base(metadataProvider, typeof(ServiceBusOperationsReceiver), PerformedOperations.IsPerformedOperationsPrimarySource)
         {
         }
     }
