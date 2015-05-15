cad6625d681bc15b4d9978d34299a08d004bf623
a.rechkalov <a.rechkalov@2gis.ru>
2015-05-15 18:19:09 +0600

    Дополнил тесты

--- Replication.Tests/Transformation/FactsTransformationTests.cs
+++ Replication.Tests/Transformation/FactsTransformationTests.cs
@@ -793,6 +793,7 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
                 yield return CaseToVerifyElementInsertion<Erm::BranchOfficeOrganizationUnit, Facts::BranchOfficeOrganizationUnit>(new Erm::BranchOfficeOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementInsertion<Erm::Category, Facts::Category>(new Erm::Category { Id = 1 });
                 yield return CaseToVerifyElementInsertion<Erm::CategoryFirmAddress, Facts::CategoryFirmAddress>(new Erm::CategoryFirmAddress { Id = 1 });
+                yield return CaseToVerifyElementInsertion<Erm::CategoryGroup, Facts::CategoryGroup>(new Erm::CategoryGroup { Id = 1 });
                 yield return CaseToVerifyElementInsertion<Erm::CategoryOrganizationUnit, Facts::CategoryOrganizationUnit>(new Erm::CategoryOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementInsertion<Erm::Client, Facts::Client>(new Erm::Client { Id = 1 });
                 yield return CaseToVerifyElementInsertion<Erm::Contact, Facts::Contact>(new Erm::Contact { Id = 1 });
@@ -801,27 +802,32 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
                 yield return CaseToVerifyElementInsertion<Erm::FirmContact, Facts::FirmContact>(new Erm::FirmContact { Id = 1, ContactType = 1, FirmAddressId = notnull });
                 yield return CaseToVerifyElementInsertion<Erm::LegalPerson, Facts::LegalPerson>(new Erm::LegalPerson { Id = 1, ClientId = notnull });
                 yield return CaseToVerifyElementInsertion<Erm::Order, Facts::Order>(new Erm::Order { Id = 1, WorkflowStepId = 4 });
+                yield return CaseToVerifyElementInsertion<Erm::Project, Facts::Project>(new Erm::Project { Id = 1 });
+                yield return CaseToVerifyElementInsertion<Erm::Territory, Facts::Territory>(new Erm::Territory { Id = 1 });
+
                 // update
                 yield return CaseToVerifyElementUpdate(new Erm::Account { Id = 1 }, new Facts::Account { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::BranchOfficeOrganizationUnit { Id = 1 }, new Facts::BranchOfficeOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::Category { Id = 1 }, new Facts::Category { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::CategoryFirmAddress { Id = 1 }, new Facts::CategoryFirmAddress { Id = 1 });
+                yield return CaseToVerifyElementUpdate(new Erm::CategoryGroup { Id = 1 }, new Facts::CategoryGroup { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::CategoryOrganizationUnit { Id = 1 }, new Facts::CategoryOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::Client { Id = 1 }, new Facts::Client { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::Contact { Id = 1 }, new Facts::Contact { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::Firm { Id = 1 }, new Facts::Firm { Id = 1 });
                 yield return CaseToVerifyElementUpdate(new Erm::FirmAddress { Id = 1 }, new Facts::FirmAddress { Id = 1 });
-                yield return
-                    CaseToVerifyElementUpdate(
-                        new Erm::FirmContact { Id = 1, ContactType = 1, FirmAddressId = notnull },
-                        new Facts::FirmContact { Id = 1, HasPhone = true, FirmAddressId = notnull });
+                yield return CaseToVerifyElementUpdate(new Erm::FirmContact { Id = 1, ContactType = 1, FirmAddressId = notnull }, new Facts::FirmContact { Id = 1, HasPhone = true, FirmAddressId = notnull });
                 yield return CaseToVerifyElementUpdate(new Erm::LegalPerson { Id = 1, ClientId = notnull }, new Facts::LegalPerson { Id = 1, ClientId = notnull });
                 yield return CaseToVerifyElementUpdate(new Erm::Order { Id = 1, WorkflowStepId = 4 }, new Facts::Order { Id = 1 });
+                yield return CaseToVerifyElementUpdate(new Erm::Project { Id = 1 }, new Facts::Project { Id = 1 });
+                yield return CaseToVerifyElementUpdate(new Erm::Territory { Id = 1 }, new Facts::Territory { Id = 1 });
+
                 // delete
                 yield return CaseToVerifyElementDeletion(new Facts::Account { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::BranchOfficeOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::Category { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::CategoryFirmAddress { Id = 1 });
+                yield return CaseToVerifyElementDeletion(new Facts::CategoryGroup { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::CategoryOrganizationUnit { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::Client { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::Contact { Id = 1 });
@@ -830,6 +836,8 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
                 yield return CaseToVerifyElementDeletion(new Facts::FirmContact { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::LegalPerson { Id = 1 });
                 yield return CaseToVerifyElementDeletion(new Facts::Order { Id = 1 });
+                yield return CaseToVerifyElementDeletion(new Facts::Project { Id = 1 });
+                yield return CaseToVerifyElementDeletion(new Facts::Territory { Id = 1 });
             }
         }
 
