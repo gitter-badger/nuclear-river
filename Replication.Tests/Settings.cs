42e102659818e8a8fcd0d8d5f82eb0fbbb8f6125
a.rechkalov <a.rechkalov@2gis.ru>
2015-05-28 15:20:24 +0600

    Исправил таймаут на длительных запросах
    В bulk-режиме всё-равно сначала выполняется SqlCommand, причём на её таймаут никак не влияет BulkCopyTimeout (потому что это другой контекст, другое подключение к базе)
    https://github.com/linq2db/linq2db/blob/57323974c0a1d75b769b4117d95292efde36dae1/Source/Data/DataConnection.cs, строка 663
    При этом у команды выставлен таймаут на 30 секунд (значение по умолчанию)
    и есть возможность задать другое значение (тот-же файл, строка 518.

--- Replication.Tests/Settings.cs
+++ Replication.Tests/Settings.cs
@@ -27,6 +27,21 @@ namespace NuClear.AdvancedSearch.Replication.Tests
             }
         }
 
+        public static int? SqlCommandTimeout
+        {
+            get
+            {
+                int timeout;
+                var timeoutValue = ConfigurationManager.AppSettings["SqlCommandTimeout"];
+                if (timeoutValue != null && int.TryParse(timeoutValue, out timeout))
+                {
+                    return timeout;
+                }
+
+                return null;
+            }
+        }
+
         private static int? sqlBulkCopyTimeout;
     }
 }
