d875fc8e93176cd65ddce95a041f17b23d3ccb91
a.rechkalov <a.rechkalov@2gis.ru>
2015-06-04 13:57:00 +0600

    Добавил несколько тестов

---
+++ Replication.Tests/Transformation/BitFactsTransformationContextTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+
+using NuClear.AdvancedSearch.Replication.CustomerIntelligence.Data.Context.Implementation;
+using NuClear.AdvancedSearch.Replication.CustomerIntelligence.Transforming;
+
+using NUnit.Framework;
+
+// ReSharper disable PossibleUnintendedReferenceComparison
+namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
+{
+    [TestFixture]
+    internal class BitFactsTransformationContextTests
+    {
+        [Test]
+        public void ShouldTransformFirmStatisticsDto()
+        {
+            var dto = new FirmStatisticsDto
+                      {
+                          ProjectId = 1,
+                          Firms = new[]
+                                  {
+                                      new FirmStatisticsDto.FirmDto
+                                      {
+                                          FirmId = 2,
+                                          CategoryId = 3,
+                                          Hits = 4,
+                                          Shows = 5
+                                      }
+                                  }
+                      };
+            var context = new BitFactsTransformationContext(dto);
+
+            var entities = context.FirmStatistics;
+            Assert.AreEqual(entities.Count(), 1);
+            Assert.AreEqual(entities.Single().ProjectId, dto.ProjectId);
+            Assert.AreEqual(entities.Single().CategoryId, dto.Firms.Single().CategoryId);
+            Assert.AreEqual(entities.Single().FirmId, dto.Firms.Single().FirmId);
+            Assert.AreEqual(entities.Single().Hits, dto.Firms.Single().Hits);
+            Assert.AreEqual(entities.Single().Shows, dto.Firms.Single().Shows);
+        }
+
+        [Test]
+        public void ShouldTransformCategoryStatisticsDto()
+        {
+            var dto = new CategoryStatisticsDto
+            {
+                ProjectId = 1,
+                Categories = new[]
+                                  {
+                                      new CategoryStatisticsDto.CategoryDto
+                                      {
+                                          CategoryId = 2,
+                                          AdvertisersCount = 3,
+                                      }
+                                  }
+            };
+            var context = new BitFactsTransformationContext(dto);
+
+            var entities = context.CategoryStatististics;
+
+            Assert.AreEqual(entities.Count(), 1);
+            Assert.AreEqual(entities.Single().ProjectId, dto.ProjectId);
+            Assert.AreEqual(entities.Single().CategoryId, dto.Categories.Single().CategoryId);
+            Assert.AreEqual(entities.Single().AdvertisersCount, dto.Categories.Single().AdvertisersCount);
+        }
+    }
+}
---
+++ Replication.Tests/Transformation/BitFactsTransformationTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+
+using Moq;
+
+using NuClear.AdvancedSearch.Replication.CustomerIntelligence.Data.Context;
+using NuClear.AdvancedSearch.Replication.CustomerIntelligence.Transforming;
+using NuClear.AdvancedSearch.Replication.CustomerIntelligence.Transforming.Operations;
+using NuClear.AdvancedSearch.Replication.Data;
+
+using NUnit.Framework;
+
+// ReSharper disable PossibleUnintendedReferenceComparison
+namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
+{
+    using Erm = CustomerIntelligence.Model.Erm;
+    using Facts = CustomerIntelligence.Model.Facts;
+    using CI = CustomerIntelligence.Model;
+
+    [TestFixture]
+    internal class BitFactsTransformationTests
+    {
+        [Test]
+        public void ShouldProduceRecalulateOperationsBothForExistedAndNewFirmStatistics()
+        {
+            var dto = new FirmStatisticsDto
+                      {
+                          ProjectId = 1,
+                          Firms = new[]
+                                  {
+                                      new FirmStatisticsDto.FirmDto
+                                      {
+                                          FirmId = 2,
+                                          CategoryId = 3,
+                                          Hits = 4,
+                                          Shows = 5
+                                      }
+                                  }
+                      };
+            var context = new Mock<IBitFactsContext>();
+            context.SetupGet(x => x.FirmStatistics).Returns(new [] {new Facts.FirmStatistics { ProjectId = 1, FirmId = 7}, new Facts.FirmStatistics { ProjectId = 2, FirmId = 8 } }.AsQueryable());
+            var transformation = new BitFactsTransformation(context.Object, Mock.Of<IDataMapper>());
+
+            var operations = transformation.Transform(dto).ToArray();
+
+            Assert.AreEqual(2, operations.Count());
+            Assert.AreEqual(2, operations.OfType<RecalculateAggregate>().Count());
+            Assert.AreEqual(2, operations.OfType<RecalculateAggregate>().Count(x => x.AggregateType == typeof(CI.Firm)));
+            Assert.AreEqual(1, operations.OfType<RecalculateAggregate>().Count(x => x.AggregateId == 2));
+            Assert.AreEqual(1, operations.OfType<RecalculateAggregate>().Count(x => x.AggregateId == 7));
+        }
+
+        [Test]
+        public void ShouldProduceRecalulateOperationsForReceivedProject()
+        {
+            var dto = new CategoryStatisticsDto
+            {
+                ProjectId = 1,
+                Categories = new[]
+                                  {
+                                      new CategoryStatisticsDto.CategoryDto
+                                      {
+                                          CategoryId = 2,
+                                          AdvertisersCount = 3,
+                                      }
+                                  }
+            };
+            var context = new Mock<IBitFactsContext>();
+            context.SetupGet(x => x.CategoryStatististics).Returns(new[] { new Facts.CategoryStatististics { ProjectId = 1, CategoryId = 7 }, new Facts.CategoryStatististics { ProjectId = 2, CategoryId = 7 } }.AsQueryable());
+            var transformation = new BitFactsTransformation(context.Object, Mock.Of<IDataMapper>());
+
+            var operations = transformation.Transform(dto).ToArray();
+
+            Assert.AreEqual(1, operations.Count());
+            Assert.AreEqual(1, operations.OfType<RecalculateAggregate>().Count());
+            Assert.AreEqual(1, operations.OfType<RecalculateAggregate>().Count(x => x.AggregateType == typeof(CI.Project)));
+            Assert.AreEqual(1, operations.OfType<RecalculateAggregate>().Count(x => x.AggregateId == 1));
+        }
+    }
+}
