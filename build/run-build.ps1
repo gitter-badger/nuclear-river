b0ea0e6a35cdea01129e40cef863734f9f294f74
m.pashuk <m.pashuk@2gis.ru>
2015-06-23 14:40:04 +0600

    buildtools update

--- build/run-build.ps1
+++ build/run-build.ps1
@@ -86,5 +86,5 @@ $Properties.EnvironmentMetadata = @{
 	& $NugetPath @('restore', $solution.FullName, '-NonInteractive', '-Verbosity', 'quiet')
 }
 
-Import-Module "$($Properties.SolutionDir)\packages\2GIS.NuClear.BuildTools.0.0.42\tools\buildtools.psm1" -DisableNameChecking -Force
+Import-Module "$($Properties.SolutionDir)\packages\2GIS.NuClear.BuildTools.0.0.45\tools\buildtools.psm1" -DisableNameChecking -Force
 Run-Build $TaskList $Properties
