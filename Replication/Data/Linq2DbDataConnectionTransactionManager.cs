2f999a04c9b903293d9e25946d6ce4d7267e98ac
a.rechkalov <a.rechkalov@2gis.ru>
2015-07-03 12:31:42 +0600

    Добавил защиту от дурака

--- Replication/Data/Linq2DbDataConnectionTransactionManager.cs
+++ Replication/Data/Linq2DbDataConnectionTransactionManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using LinqToDB.Data;
 
@@ -10,34 +11,57 @@ namespace NuClear.AdvancedSearch.Replication.Data
     {
         private const IsolationLevel DefaultIsolationLevel = IsolationLevel.ReadCommitted;
         private readonly IEnumerable<DataConnection> _connections;
+        private bool _transactionIsActive;
 
         public Linq2DbDataConnectionTransactionManager(IEnumerable<DataConnection> connections)
         {
             _connections = connections;
+            _transactionIsActive = false;
         }
 
         public void BeginTransaction()
         {
+            if (_transactionIsActive)
+            {
+                throw new InvalidOperationException("Transaction have already been started");
+            }
+
             foreach (var dataConnection in _connections)
             {
                 dataConnection.BeginTransaction(DefaultIsolationLevel);
             }
+
+            _transactionIsActive = true;
         }
 
         public void CommitTransaction()
         {
+            if (!_transactionIsActive)
+            {
+                throw new InvalidOperationException("No active transaction");
+            } 
+            
             foreach (var dataConnection in _connections)
             {
                 dataConnection.CommitTransaction();
             }
+
+            _transactionIsActive = false;
         }
 
         public void RollbackTransaction()
         {
+            if (!_transactionIsActive)
+            {
+                throw new InvalidOperationException("No active transaction");
+            } 
+            
             foreach (var dataConnection in _connections)
             {
                 dataConnection.RollbackTransaction();
             }
+
+            _transactionIsActive = false;
         }
     }
 }
