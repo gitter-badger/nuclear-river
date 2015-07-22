af787bbda12e0c0e6190eb73964c142b85674c97
a.rechkalov <a.rechkalov@2gis.ru>
2015-07-22 13:52:29 +0600

    Удалил старое

--- Telemetry/Metadata/IGraphiteCounterMetadata.cs
+++
@@ -1,9 +0,0 @@
-namespace NuClear.Telemetry
-{
-    // TODO {a.rechkalov, 22.06.2015}: MetadataSource из ERM
-    public interface IGraphiteCounterMetadata
-    {
-        GraphiteMetadataElement Get<T>()
-            where T : TelemetryIdentityBase<T>, new();
-    }
-}
