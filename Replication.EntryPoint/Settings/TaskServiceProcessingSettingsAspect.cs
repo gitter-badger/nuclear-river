0fe820a022928f27866d3c6f2c67ddf017da3c6e
m.pashuk <m.pashuk@2gis.ru>
2015-07-22 15:27:34 +0600

    Добавлена настройка MisfireThreshhold из quartz

--- Replication.EntryPoint/Settings/TaskServiceProcessingSettingsAspect.cs
+++ Replication.EntryPoint/Settings/TaskServiceProcessingSettingsAspect.cs
@@ -1,4 +1,7 @@
-﻿using NuClear.Jobs.Settings;
+﻿using System;
+using System.Globalization;
+
+using NuClear.Jobs.Settings;
 using NuClear.Settings;
 using NuClear.Settings.API;
 
@@ -9,6 +12,7 @@ namespace NuClear.AdvancedSearch.Replication.EntryPoint.Settings
         private readonly IntSetting _maxWorkingThreads = ConfigFileSetting.Int.Required("MaxWorkingThreads");
         private readonly EnumSetting<JobStoreType> _jobStoreType = ConfigFileSetting.Enum.Required<JobStoreType>("JobStoreType");
         private readonly StringSetting _schedulerName = ConfigFileSetting.String.Required("SchedulerName");
+        private readonly StringSetting _misfireThreshold = ConfigFileSetting.String.Required("MisfireThreshold");
 
         int ITaskServiceProcessingSettings.MaxWorkingThreads
         {
@@ -23,6 +27,11 @@ namespace NuClear.AdvancedSearch.Replication.EntryPoint.Settings
         string ITaskServiceProcessingSettings.SchedulerName
         {
             get { return _schedulerName.Value; }
+        }
+
+        public TimeSpan MisfireThreshold
+        {
+            get { return TimeSpan.Parse(_misfireThreshold.Value, CultureInfo.InvariantCulture); }
         }
     }
 }
