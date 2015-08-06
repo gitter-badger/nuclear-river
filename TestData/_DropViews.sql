80edd8ee4c86fd055a22e618b7f0813814fca37f
m.pashuk <m.pashuk@2gis.ru>
2015-08-06 18:48:59 +0600

    изменения в билде + quartz

---
+++ TestData/_DropViews.sql
@@ -0,0 +1,6 @@
+-- drop views
+if object_id('ERM.ViewClient', 'view') is not null drop view ERM.ViewClient;
+if object_id('BIT.FirmCategory', 'view') is not null drop view BIT.FirmCategory;
+if object_id('CustomerIntelligence.FirmCategory', 'view') is not null drop view CustomerIntelligence.FirmCategory;
+go
+
