41a36422765f4dcaca2d00993536c93267603e88
a.rechkalov <a.rechkalov@2gis.ru>
2015-09-14 14:29:40 +0600

    Выкосил MemoryMockQuery из тестов преобразования Erm -> Fact

--- Replication.Tests/Transformation/ErmMapToFactsSpecsTests.Activity.cs
+++ Replication.Tests/Transformation/ErmMapToFactsSpecsTests.Activity.cs
@@ -47,35 +47,34 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
             const long ClientId = 222;
             var date = DateTimeOffset.Parse("2015-01-01");
 
-            var query = new MemoryMockQuery(
-                new TActivity { Id = 1, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
-                new TActivity { Id = 2, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
-                new TActivity { Id = 3, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
-                new TActivity { Id = 4, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
-                new TActivityReference { ActivityId = 1, Reference = RegardingObjectReference, ReferencedObjectId = ClientId, ReferencedType = EntityTypeIds.Client },
-                new TActivityReference { ActivityId = 1, Reference = RegardingObjectReference, ReferencedObjectId = FirmId, ReferencedType = EntityTypeIds.Firm },
-                new TActivityReference { ActivityId = 2, Reference = RegardingObjectReference, ReferencedObjectId = FirmId, ReferencedType = EntityTypeIds.Firm },
-                new TActivityReference { ActivityId = 3, Reference = RegardingObjectReference, ReferencedObjectId = ClientId, ReferencedType = EntityTypeIds.Client });
+            SourceDb.Has(new TActivity { Id = 1, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
+                         new TActivity { Id = 2, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
+                         new TActivity { Id = 3, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted },
+                         new TActivity { Id = 4, ModifiedOn = date, IsActive = true, Status = ActivityStatusCompleted })
+                    .Has(new TActivityReference { ActivityId = 1, Reference = RegardingObjectReference, ReferencedObjectId = ClientId, ReferencedType = EntityTypeIds.Client },
+                         new TActivityReference { ActivityId = 1, Reference = RegardingObjectReference, ReferencedObjectId = FirmId, ReferencedType = EntityTypeIds.Firm },
+                         new TActivityReference { ActivityId = 2, Reference = RegardingObjectReference, ReferencedObjectId = FirmId, ReferencedType = EntityTypeIds.Firm },
+                         new TActivityReference { ActivityId = 3, Reference = RegardingObjectReference, ReferencedObjectId = ClientId, ReferencedType = EntityTypeIds.Client });
 
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(1).Count(), Is.EqualTo(1));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(1).Single().ClientId, Is.EqualTo(ClientId));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(1).Single().FirmId, Is.EqualTo(FirmId));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(1).Single().ModifiedOn, Is.EqualTo(date));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(1).Count(), Is.EqualTo(1));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(1).Single().ClientId, Is.EqualTo(ClientId));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(1).Single().FirmId, Is.EqualTo(FirmId));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(1).Single().ModifiedOn, Is.EqualTo(date));
 
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(2).Count(), Is.EqualTo(1));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(2).Single().ClientId, Is.Null);
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(2).Single().FirmId, Is.EqualTo(FirmId));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(2).Single().ModifiedOn, Is.EqualTo(date));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(2).Count(), Is.EqualTo(1));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(2).Single().ClientId, Is.Null);
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(2).Single().FirmId, Is.EqualTo(FirmId));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(2).Single().ModifiedOn, Is.EqualTo(date));
 
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(3).Count(), Is.EqualTo(1));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(3).Single().ClientId, Is.EqualTo(ClientId));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(3).Single().FirmId, Is.Null);
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(3).Single().ModifiedOn, Is.EqualTo(date));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(3).Count(), Is.EqualTo(1));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(3).Single().ClientId, Is.EqualTo(ClientId));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(3).Single().FirmId, Is.Null);
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(3).Single().ModifiedOn, Is.EqualTo(date));
 
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(4).Count(), Is.EqualTo(1));
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(4).Single().ClientId, Is.Null);
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(4).Single().FirmId, Is.Null);
-            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(query).ById(4).Single().ModifiedOn, Is.EqualTo(date));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(4).Count(), Is.EqualTo(1));
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(4).Single().ClientId, Is.Null);
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(4).Single().FirmId, Is.Null);
+            Assert.That(Specs.Map.Erm.ToFacts.Activities.Map(Query).ById(4).Single().ModifiedOn, Is.EqualTo(date));
         }
    }
 }
--- Replication.Tests/Transformation/ErmMapToFactsSpecsTests.cs
+++ Replication.Tests/Transformation/ErmMapToFactsSpecsTests.cs
@@ -20,86 +20,87 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
         [Test]
         public void ShouldTransformAccount()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Account { Id = 1, Balance = 123.45m, LegalPersonId = 2 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.Accounts.Map(q).ById(1), Inquire(new Facts::Account { Id = 1, Balance = 123.45m, LegalPersonId = 2 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.Accounts.Map(q).ById(1), new Facts::Account { Id = 1, Balance = 123.45m, LegalPersonId = 2 });
         }
 
         [Test]
         public void ShouldTransformBranchOfficeOrganizationUnit()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::BranchOfficeOrganizationUnit { Id = 1, OrganizationUnitId = 2 });
             
-            Transformation.Create(query)
+            Transformation.Create(Query)
                           .VerifyTransform(q => Specs.Map.Erm.ToFacts.BranchOfficeOrganizationUnits.Map(q).ById(1),
-                                           Inquire(new Facts::BranchOfficeOrganizationUnit { Id = 1, OrganizationUnitId = 2 }));
+                                           new Facts::BranchOfficeOrganizationUnit { Id = 1, OrganizationUnitId = 2 });
         }
 
         [Test]
         public void ShouldTransformCategory()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Category { Id = 1, Level = 2, ParentId = 3 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.Categories.Map(q).ById(1), Inquire(new Facts::Category { Id = 1, Level = 2, ParentId = 3 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.Categories.Map(q).ById(1), new Facts::Category { Id = 1, Level = 2, ParentId = 3 });
         }
 
         [Test]
         public void ShouldTransformCategoryFirmAddress()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::CategoryFirmAddress { Id = 1, CategoryId = 2, FirmAddressId = 3 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryFirmAddresses.Map(q).ById(1), Inquire(new Facts::CategoryFirmAddress { Id = 1, CategoryId = 2, FirmAddressId = 3 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryFirmAddresses.Map(q).ById(1), new Facts::CategoryFirmAddress { Id = 1, CategoryId = 2, FirmAddressId = 3 });
         }
 
         [Test]
         public void ShouldTransformCategoryGroup()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::CategoryGroup { Id = 1, Name = "name", Rate = 1 });
             
-            Transformation.Create(query)
-                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryGroups.Map(q).ById(1), Inquire(new Facts::CategoryGroup { Id = 1, Name = "name", Rate = 1 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryGroups.Map(q).ById(1), new Facts::CategoryGroup { Id = 1, Name = "name", Rate = 1 });
         }
 
         [Test]
         public void ShouldTransformCategoryOrganizationUnit()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::CategoryOrganizationUnit { Id = 1, CategoryId = 2, CategoryGroupId = 3, OrganizationUnitId = 4 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryOrganizationUnits.Map(q).ById(1), Inquire(new Facts::CategoryOrganizationUnit { Id = 1, CategoryId = 2, CategoryGroupId = 3, OrganizationUnitId = 4 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(q => Specs.Map.Erm.ToFacts.CategoryOrganizationUnits.Map(q).ById(1),
+                                           new Facts::CategoryOrganizationUnit { Id = 1, CategoryId = 2, CategoryGroupId = 3, OrganizationUnitId = 4 });
         }
 
         [Test]
         public void ShouldTransformClient()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Client { Id = 1, Name = "client", LastDisqualifyTime = Date },
                 new Erm::Client { Id = 2, MainPhoneNumber = "phone" },
                 new Erm::Client { Id = 3, AdditionalPhoneNumber1 = "phone" },
                 new Erm::Client { Id = 4, AdditionalPhoneNumber2 = "phone" },
                 new Erm::Client { Id = 5, Website = "site" });
 
-            Transformation.Create(query)
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(1), Inquire(new Facts::Client { Id = 1, Name = "client", LastDisqualifiedOn = Date }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(2), Inquire(new Facts::Client { Id = 2, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(3), Inquire(new Facts::Client { Id = 3, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(4), Inquire(new Facts::Client { Id = 4, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(5), Inquire(new Facts::Client { Id = 5, HasWebsite = true }));
+            Transformation.Create(Query)
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(1), new Facts::Client { Id = 1, Name = "client", LastDisqualifiedOn = Date })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(2), new Facts::Client { Id = 2, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(3), new Facts::Client { Id = 3, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(4), new Facts::Client { Id = 4, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Clients.Map(x).ById(5), new Facts::Client { Id = 5, HasWebsite = true });
         }
 
         [Test]
         public void ShouldTransformContact()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Contact { Id = 1, IsFired = true, ClientId = 2 },
                 new Erm::Contact { Id = 2, Role = 200000 },
                 new Erm::Contact { Id = 3, Role = 200001 },
@@ -110,36 +111,36 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
                 new Erm::Contact { Id = 8, AdditionalPhoneNumber = "phone" },
                 new Erm::Contact { Id = 9, Website = "site" });
 
-            Transformation.Create(query)
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(1), Inquire(new Facts::Contact { Id = 1, IsFired = true, ClientId = 2 }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(2), Inquire(new Facts::Contact { Id = 2, Role = 1 }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(3), Inquire(new Facts::Contact { Id = 3, Role = 2 }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(4), Inquire(new Facts::Contact { Id = 4, Role = 3 }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(5), Inquire(new Facts::Contact { Id = 5, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(6), Inquire(new Facts::Contact { Id = 6, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(7), Inquire(new Facts::Contact { Id = 7, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(8), Inquire(new Facts::Contact { Id = 8, HasPhone = true }))
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(9), Inquire(new Facts::Contact { Id = 9, HasWebsite = true }));
+            Transformation.Create(Query)
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(1), new Facts::Contact { Id = 1, IsFired = true, ClientId = 2 })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(2), new Facts::Contact { Id = 2, Role = 1 })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(3), new Facts::Contact { Id = 3, Role = 2 })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(4), new Facts::Contact { Id = 4, Role = 3 })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(5), new Facts::Contact { Id = 5, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(6), new Facts::Contact { Id = 6, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(7), new Facts::Contact { Id = 7, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(8), new Facts::Contact { Id = 8, HasPhone = true })
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Contacts.Map(x).ById(9), new Facts::Contact { Id = 9, HasWebsite = true });
         }
 
         [Test]
         public void ShouldTransformFirm()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Firm { Id = 1, Name = "firm", CreatedOn = Date, LastDisqualifyTime = Date.AddDays(1), ClientId = 2, OrganizationUnitId = 3, TerritoryId = 4, OwnerId = 5 });
 
-            Transformation.Create(query)
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Firms.Map(x).ById(1), Inquire(new Facts::Firm { Id = 1, Name = "firm", CreatedOn = Date, LastDisqualifiedOn = Date.AddDays(1), ClientId = 2, OrganizationUnitId = 3, TerritoryId = 4, OwnerId = 5 }));
+            Transformation.Create(Query)
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Firms.Map(x).ById(1), new Facts::Firm { Id = 1, Name = "firm", CreatedOn = Date, LastDisqualifiedOn = Date.AddDays(1), ClientId = 2, OrganizationUnitId = 3, TerritoryId = 4, OwnerId = 5 });
         }
 
         [Test]
         public void ShouldTransformFirmAddress()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::FirmAddress { Id = 1, FirmId = 2 });
 
-            Transformation.Create(query)
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmAddresses.Map(x).ById(1), Inquire(new Facts::FirmAddress { Id = 1, FirmId = 2 }));
+            Transformation.Create(Query)
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmAddresses.Map(x).ById(1), new Facts::FirmAddress { Id = 1, FirmId = 2 });
         }
 
         [Test]
@@ -147,61 +148,61 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
         {
             const long NotNull = 123;
 
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::FirmContact { Id = 1, ContactType = 1, FirmAddressId = NotNull },
                 new Erm::FirmContact { Id = 2, ContactType = 1, FirmAddressId = null },
                 new Erm::FirmContact { Id = 3, ContactType = 2, FirmAddressId = NotNull },
                 new Erm::FirmContact { Id = 4, ContactType = 4, FirmAddressId = NotNull });
 
-            Transformation.Create(query)
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(1), Inquire(new Facts::FirmContact { Id = 1, HasPhone = true, FirmAddressId = NotNull }))
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(2), Inquire<Facts::FirmContact>())
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(3), Inquire<Facts::FirmContact>())
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(4), Inquire(new Facts::FirmContact { Id = 4, HasWebsite = true, FirmAddressId = NotNull }));
+            Transformation.Create(Query)
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(1), new Facts::FirmContact { Id = 1, HasPhone = true, FirmAddressId = NotNull })
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(2), Enumerable.Empty<Facts::FirmContact>())
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(3), Enumerable.Empty<Facts::FirmContact>())
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.FirmContacts.Map(x).ById(4), new Facts::FirmContact { Id = 4, HasWebsite = true, FirmAddressId = NotNull });
         }
 
         [Test]
         public void ShouldTransformLegalPerson()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::LegalPerson { Id = 1, ClientId = 2 },
                 new Erm::LegalPerson { Id = 2, ClientId = null });
 
-            Transformation.Create(query)
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.LegalPersons.Map(x).ById(1), Inquire(new Facts::LegalPerson { Id = 1, ClientId = 2 }))
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.LegalPersons.Map(x).ById(2), Inquire<Facts::LegalPerson>());
+            Transformation.Create(Query)
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.LegalPersons.Map(x).ById(1), new Facts::LegalPerson { Id = 1, ClientId = 2 })
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.LegalPersons.Map(x).ById(2), Enumerable.Empty<Facts::LegalPerson>());
         }
 
         [Test]
         public void ShouldTransformOrder()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Order { Id = 1, EndDistributionDateFact = Date, WorkflowStepId = 1, FirmId = 2 },
                 new Erm::Order { Id = 2, EndDistributionDateFact = Date, WorkflowStepId = 4 /* on termination*/, FirmId = 2 });
 
-            Transformation.Create(query)
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Orders.Map(x).ById(1), Inquire<Facts::Order>())
-                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Orders.Map(x).ById(2), Inquire(new Facts::Order { Id = 2, EndDistributionDateFact = Date, FirmId = 2 }));
+            Transformation.Create(Query)
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Orders.Map(x).ById(1), Enumerable.Empty<Facts::Order>())
+                .VerifyTransform(x => Specs.Map.Erm.ToFacts.Orders.Map(x).ById(2), new Facts::Order { Id = 2, EndDistributionDateFact = Date, FirmId = 2 });
         }
 
         [Test]
         public void ShouldTransformProject()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Project { Id = 1, Name = "name", OrganizationUnitId = 2 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Projects.Map(x).ById(1), Inquire(new Facts::Project { Id = 1, Name = "name", OrganizationUnitId = 2 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Projects.Map(x).ById(1), new Facts::Project { Id = 1, Name = "name", OrganizationUnitId = 2 });
         }
 
         [Test]
         public void ShouldTransformTerritory()
         {
-            var query = new MemoryMockQuery(
+            SourceDb.Has(
                 new Erm::Territory { Id = 1, Name = "name", OrganizationUnitId = 2 });
 
-            Transformation.Create(query)
-                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Territories.Map(x).ById(1), Inquire(new Facts::Territory { Id = 1, Name = "name", OrganizationUnitId = 2 }));
+            Transformation.Create(Query)
+                          .VerifyTransform(x => Specs.Map.Erm.ToFacts.Territories.Map(x).ById(1), new Facts::Territory { Id = 1, Name = "name", OrganizationUnitId = 2 });
         }
 
         #region Transformation
@@ -223,6 +224,12 @@ namespace NuClear.AdvancedSearch.Replication.Tests.Transformation
             public Transformation VerifyTransform<T>(Func<IQuery, IEnumerable<T>> reader, IEnumerable<T> expected, string message = null)
             {
                 VerifyTransform(reader, expected, x => x, message);
+                return this;
+            }
+
+            public Transformation VerifyTransform<T>(Func<IQuery, IEnumerable<T>> reader, params T[] expected)
+            {
+                VerifyTransform(reader, expected, x => x, null);
                 return this;
             }
 
