9b0da931ce4e125d983fd473a68d33cf46c16ecc
a.rechkalov <a.rechkalov@2gis.ru>
2015-10-06 11:24:11 +0600

    Добавил логирование дублей

--- Replication.OperationsProcessing/Performance/ServiceBusOperationsReceiverTelemetryWrapper.cs
+++ Replication.OperationsProcessing/Performance/ServiceBusOperationsReceiverTelemetryWrapper.cs
@@ -6,6 +6,7 @@ using NuClear.Messaging.API.Receivers;
 using NuClear.OperationsProcessing.Transports.ServiceBus.Primary;
 using NuClear.Telemetry;
 using NuClear.Telemetry.Probing;
+using NuClear.Tracing.API;
 
 namespace NuClear.Replication.OperationsProcessing.Performance
 {
@@ -13,11 +14,13 @@ namespace NuClear.Replication.OperationsProcessing.Performance
     {
         private readonly IMessageReceiver _receiver;
         private readonly ITelemetryPublisher _telemetryPublisher;
+        private readonly ITracer _tracer;
 
-        public ServiceBusOperationsReceiverTelemetryWrapper(ServiceBusOperationsReceiver receiver, ITelemetryPublisher telemetryPublisher)
+        public ServiceBusOperationsReceiverTelemetryWrapper(ServiceBusOperationsReceiver receiver, ITelemetryPublisher telemetryPublisher, ITracer tracer)
         {
             _receiver = receiver;
             _telemetryPublisher = telemetryPublisher;
+            _tracer = tracer;
         }
 
         public IReadOnlyList<IMessage> Peek()
@@ -27,6 +30,13 @@ namespace NuClear.Replication.OperationsProcessing.Performance
                 var messages = _receiver.Peek();
                 var serviceBusMessageCount = messages.Cast<ServiceBusPerformedOperationsMessage>().Sum(x => x.Operations.Count());
                 _telemetryPublisher.Publish<ErmReceivedUseCaseCountIdentity>(serviceBusMessageCount);
+
+                var dublicates = messages.GroupBy(x => x.Id).Where(group => group.Count() > 1).ToArray();
+                if (dublicates.Any())
+                {
+                    _tracer.Warn(string.Format("found message dublicates: {0}", string.Join(", ", dublicates.Select(x => x.Key.ToString()))));
+                }
+
                 return messages;
             }
         }
