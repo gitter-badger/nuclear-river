c9e48ec711719bbee736196d7b3a6e422ff70669
a.rechkalov <a.rechkalov@2gis.ru>
2015-10-23 16:12:59 +0600

    Почистил зависимости

--- Replication/Tests/Replication.Bulk.Tests/BulkReplication.cs
+++ Replication/Tests/Replication.Bulk.Tests/BulkReplication.cs
@@ -9,32 +9,31 @@ namespace NuClear.AdvancedSearch.Replication.Bulk.Tests
 	[TestFixture]
 	public sealed class BulkReplication
 	{
-	    public static IEnumerable<TestCaseData> FactReplication
-            = Program.GetReplicationActions("-fact").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
+	    public static IEnumerable<TestCaseData> GetFactReplication()
+            => Program.GetReplicationActions("-fact").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
 
-	    public static IEnumerable<TestCaseData> CiReplication
-            = Program.GetReplicationActions("-ci").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
+	    public static IEnumerable<TestCaseData> GetCiReplication()
+            => Program.GetReplicationActions("-ci").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
 
-	    public static IEnumerable<TestCaseData> StatisticsReplication
-            = Program.GetReplicationActions("-statistics").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
-
+	    public static IEnumerable<TestCaseData> GetStatisticsReplication()
+	        => Program.GetReplicationActions("-statistics").Select(tuple => new TestCaseData(tuple.Item2).SetName(tuple.Item1));
 
         [Ignore("Use for bulk replication")]
-        [TestCaseSource(nameof(FactReplication), Category = "FactReplication")]
+        [TestCaseSource(nameof(GetFactReplication))]
         public void FactReplicationTest(Action replicationAction)
         {
             replicationAction.Invoke();
         }
 
         [Ignore("Use for bulk replication")]
-        [TestCaseSource(nameof(CiReplication), Category = "CiReplication")]
+        [TestCaseSource(nameof(GetCiReplication))]
         public void CiReplicationTest(Action replicationAction)
         {
             replicationAction.Invoke();
         }
 
         [Ignore("Use for bulk replication")]
-        [TestCaseSource(nameof(StatisticsReplication), Category = "StatisticsReplication")]
+        [TestCaseSource(nameof(GetStatisticsReplication))]
         public void StatisticsReplicationTest(Action replicationAction)
         {
             replicationAction.Invoke();
