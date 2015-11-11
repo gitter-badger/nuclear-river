ea2c06a6536d958612da857757cade71ab609cb5
Denis Ivanov <d.ivanov@2gis.ru>
2015-11-11 11:12:17 +0600

    unused dependencies removed

--- Replication/Replication.Core/Aggregates/AggregateProcessor.cs
+++ Replication/Replication.Core/Aggregates/AggregateProcessor.cs
@@ -3,7 +3,6 @@ using System.Linq;
 
 using NuClear.AdvancedSearch.Common.Metadata;
 using NuClear.AdvancedSearch.Common.Metadata.Elements;
-using NuClear.AdvancedSearch.Common.Metadata.Features;
 using NuClear.AdvancedSearch.Common.Metadata.Model;
 using NuClear.Replication.Core.API;
 using NuClear.Replication.Core.API.Aggregates;
