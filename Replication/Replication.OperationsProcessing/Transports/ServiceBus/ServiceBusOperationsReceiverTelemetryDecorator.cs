63ca2b15597d3df2d30b967ffab86ce62300996f
a.rechkalov <a.rechkalov@2gis.ru>
2015-11-12 14:20:25 +0600

    Добавил дедупликацию

--- Replication/Replication.OperationsProcessing/Transports/ServiceBus/ServiceBusOperationsReceiverTelemetryDecorator.cs
+++ Replication/Replication.OperationsProcessing/Transports/ServiceBus/ServiceBusOperationsReceiverTelemetryDecorator.cs
@@ -7,6 +7,7 @@ using NuClear.OperationsProcessing.Transports.ServiceBus.Primary;
 using NuClear.Replication.OperationsProcessing.Identities.Telemetry;
 using NuClear.Telemetry;
 using NuClear.Telemetry.Probing;
+using NuClear.Tracing.API;
 
 namespace NuClear.Replication.OperationsProcessing.Transports.ServiceBus
 {
@@ -14,11 +15,13 @@ namespace NuClear.Replication.OperationsProcessing.Transports.ServiceBus
     {
         private readonly IMessageReceiver _receiver;
         private readonly ITelemetryPublisher _telemetryPublisher;
+        private readonly ITracer _tracer;
 
-        public ServiceBusOperationsReceiverTelemetryDecorator(ServiceBusOperationsReceiver receiver, ITelemetryPublisher telemetryPublisher)
+        public ServiceBusOperationsReceiverTelemetryDecorator(ServiceBusOperationsReceiver receiver, ITelemetryPublisher telemetryPublisher, ITracer tracer)
         {
             _receiver = receiver;
             _telemetryPublisher = telemetryPublisher;
+            _tracer = tracer;
         }
 
         public IReadOnlyList<IMessage> Peek()
@@ -26,9 +29,16 @@ namespace NuClear.Replication.OperationsProcessing.Transports.ServiceBus
             using (Probe.Create("Peek Erm Operations"))
             {
                 var messages = _receiver.Peek();
-
                 var serviceBusMessageCount = messages.Cast<ServiceBusPerformedOperationsMessage>().Sum(x => x.Operations.Count());
                 _telemetryPublisher.Publish<ErmReceivedUseCaseCountIdentity>(serviceBusMessageCount);
+
+                var dublicates = messages.GroupBy(x => x.Id).Where(group => group.Count() > 1).ToArray();
+                if (dublicates.Any())
+                {
+                    var dublicateIds = string.Join(", ", dublicates.Select(x => x.Key.ToString()));
+                    _tracer.Warn($"removing tacked use case dublicates: {dublicateIds}");
+                    return messages.GroupBy(x => x.Id).Select(group => group.First()).ToArray();
+                }
 
                 return messages;
             }
