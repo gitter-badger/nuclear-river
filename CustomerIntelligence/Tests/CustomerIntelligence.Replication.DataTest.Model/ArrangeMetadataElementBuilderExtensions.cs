8341517871ddfa7bc5943c65095258dee1bf55e3
a.rechkalov <a.rechkalov@2gis.ru>
2015-11-30 11:19:14 +0600

    Перенёс методы-расширения из DataTest.Metamodel

---
+++ CustomerIntelligence/Tests/CustomerIntelligence.Replication.DataTest.Model/ArrangeMetadataElementBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using NuClear.DataTest.Metamodel.Dsl;
+
+namespace CustomerIntelligence.Replication.DataTest.Model
+{
+    public static class ArrangeMetadataElementBuilderExtensions
+    {
+        public static ArrangeMetadataElementBuilder CustomerIntelligence(this ArrangeMetadataElementBuilder builder, params object[] data)
+            => builder.WithFeatures(new ArrangeMetadataElement.ContextStateFeature("CustomerIntelligence", data));
+
+        public static ArrangeMetadataElementBuilder Fact(this ArrangeMetadataElementBuilder builder, params object[] data)
+            => builder.WithFeatures(new ArrangeMetadataElement.ContextStateFeature("Facts", data));
+
+        public static ArrangeMetadataElementBuilder Erm(this ArrangeMetadataElementBuilder builder, params object[] data)
+            => builder.WithFeatures(new ArrangeMetadataElement.ContextStateFeature("Erm", data));
+
+        public static ArrangeMetadataElementBuilder Bit(this ArrangeMetadataElementBuilder builder, params object[] data)
+            => builder.WithFeatures(new ArrangeMetadataElement.ContextStateFeature("Bit", data));
+
+        public static ArrangeMetadataElementBuilder Statistics(this ArrangeMetadataElementBuilder builder, params object[] data)
+            => builder.WithFeatures(new ArrangeMetadataElement.ContextStateFeature("Statistics", data));
+    }
+}
