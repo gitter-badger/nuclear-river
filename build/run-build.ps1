5694e66013065ed83ca5ff3b869d1923bf977309
a.rechkalov <a.rechkalov@2gis.ru>
2015-12-04 16:39:41 +0600

    v0.0.3

--- build/run-build.ps1
+++ build/run-build.ps1
@@ -19,7 +19,7 @@ $ErrorActionPreference = 'Stop'
 #------------------------------
 cls
 
-$Properties.SemanticVersion = '0.0.2'
+$Properties.SemanticVersion = '0.0.3'
 $Properties.SolutionDir = Join-Path $PSScriptRoot '..'
 $Properties.BuildFile = Join-Path $PSScriptRoot 'default.ps1'
 
