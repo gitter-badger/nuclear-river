5b0eb68062fcc5f03a7acbd8fb90018961214336
a.rechkalov <a.rechkalov@2gis.ru>
2015-12-08 12:39:06 +0600

    Обновил тест

--- CustomerIntelligence/Tests/CustomerIntelligence.Replication.Tests/Transformation/StatisticsImporterTests.cs
+++ CustomerIntelligence/Tests/CustomerIntelligence.Replication.Tests/Transformation/StatisticsImporterTests.cs
@@ -18,11 +18,13 @@ using Bit = NuClear.CustomerIntelligence.Domain.Model.Bit;
 namespace NuClear.CustomerIntelligence.Replication.Tests.Transformation
 {
     [TestFixture]
-    internal class StatisticsImporterTests
+    internal class StatisticsImporterTests : DataFixtureBase
     {
         [Test]
         public void ShouldProduceCalculateStatisticsOperationForFirmStatisticsDto()
         {
+            // Arrange
+            var repositoryFactory = new VerifiableRepositoryFactory();
             var dto = new FirmStatisticsDto
                       {
                           ProjectId = 1,
@@ -44,31 +46,39 @@ namespace NuClear.CustomerIntelligence.Replication.Tests.Transformation
                                   }
                       };
 
-            var query = new MemoryMockQuery(
-                new Bit::FirmCategoryStatistics { ProjectId = 1, FirmId = 7 },
-                new Bit::FirmCategoryStatistics { ProjectId = 2, FirmId = 8 });
+            SourceDb.Has(new Bit::FirmCategoryStatistics { ProjectId = 1, FirmId = 7 },
+                         new Bit::FirmCategoryStatistics { ProjectId = 2, FirmId = 8 });
 
             var metadataSource = new ImportStatisticsMetadataSource();
             IMetadataElement importStatisticsMetadata;
             if (!metadataSource.Metadata.Values.TryGetElementById(new Uri(typeof(FirmStatisticsDto).Name, UriKind.Relative), out importStatisticsMetadata))
             {
-                throw new NotSupportedException(string.Format("The aggregate of type '{0}' is not supported.", typeof(FirmStatisticsDto)));
+                throw new NotSupportedException($"The aggregate of type '{typeof(FirmStatisticsDto).Name}' is not supported.");
             }
 
             var importer = new StatisticsFactImporter<Bit::FirmCategoryStatistics, FirmStatisticsDto>(
                 (ImportStatisticsMetadata<Bit::FirmCategoryStatistics, FirmStatisticsDto>)importStatisticsMetadata,
-                query,
-                Mock.Of<IBulkRepository<Bit::FirmCategoryStatistics>>());
+                Query,
+                repositoryFactory.Create<Bit::FirmCategoryStatistics>());
 
+            // Act
             var operations = importer.Import(dto).ToArray();
 
+            // Assert
             Assert.That(operations.Count(), Is.EqualTo(1));
-            Assert.That(operations.Count(), Is.EqualTo(1));
+            repositoryFactory.Verify<Bit::FirmCategoryStatistics>(
+                m => m.Delete(It.Is(Predicate.Match(new Bit::FirmCategoryStatistics { ProjectId = 1, FirmId = 7 }))),
+                Times.AtLeastOnce);
+            repositoryFactory.Verify<Bit::FirmCategoryStatistics>(
+                m => m.Add(It.Is(Predicate.Match(new Bit::FirmCategoryStatistics { ProjectId = 1, FirmId = 2, CategoryId = 3, Hits = 4, Shows = 5 }))),
+                Times.AtLeastOnce);
         }
 
         [Test]
         public void ShouldProduceCalculateStatisticsOperationForCategoryStatisticsDto()
         {
+            // Arrange
+            var repositoryFactory = new VerifiableRepositoryFactory();
             var dto = new CategoryStatisticsDto
             {
                 ProjectId = 1,
@@ -81,26 +91,32 @@ namespace NuClear.CustomerIntelligence.Replication.Tests.Transformation
                                       }
                                   }
             };
-            var query = new MemoryMockQuery(
-                new Bit::ProjectCategoryStatistics { ProjectId = 1, CategoryId = 7 },
-                new Bit::ProjectCategoryStatistics { ProjectId = 2, CategoryId = 7 });
+            SourceDb.Has(new Bit::ProjectCategoryStatistics { ProjectId = 1, CategoryId = 7 },
+                         new Bit::ProjectCategoryStatistics { ProjectId = 2, CategoryId = 7 });
 
             var metadataSource = new ImportStatisticsMetadataSource();
             IMetadataElement importStatisticsMetadata;
             if (!metadataSource.Metadata.Values.TryGetElementById(new Uri(typeof(CategoryStatisticsDto).Name, UriKind.Relative), out importStatisticsMetadata))
             {
-                throw new NotSupportedException(string.Format("The aggregate of type '{0}' is not supported.", typeof(CategoryStatisticsDto)));
+                throw new NotSupportedException($"The aggregate of type '{typeof(CategoryStatisticsDto).Name}' is not supported.");
             }
 
             var importer = new StatisticsFactImporter<Bit::ProjectCategoryStatistics, CategoryStatisticsDto>(
                 (ImportStatisticsMetadata<Bit::ProjectCategoryStatistics, CategoryStatisticsDto>)importStatisticsMetadata,
-                query,
-                Mock.Of<IBulkRepository<Bit::ProjectCategoryStatistics>>());
+                Query,
+                repositoryFactory.Create<Bit::ProjectCategoryStatistics>());
 
+            // Act
             var operations = importer.Import(dto).ToArray();
 
+            // Assert
             Assert.That(operations.Count(), Is.EqualTo(1));
-            Assert.That(operations.Count(), Is.EqualTo(1));
+            repositoryFactory.Verify<Bit::ProjectCategoryStatistics>(
+                m => m.Delete(It.Is(Predicate.Match(new Bit::ProjectCategoryStatistics { ProjectId = 1, CategoryId = 7 }))),
+                Times.AtLeastOnce);
+            repositoryFactory.Verify<Bit::ProjectCategoryStatistics>(
+                m => m.Add(It.Is(Predicate.Match(new Bit::ProjectCategoryStatistics { ProjectId = 1, CategoryId = 2, AdvertisersCount = 3 }))),
+                Times.AtLeastOnce);
         }
     }
 }
