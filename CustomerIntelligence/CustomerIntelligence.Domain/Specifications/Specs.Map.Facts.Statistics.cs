22aec7ab5ba46a81cf1c1594de9b8c059f3e60e7
m.pashuk <m.pashuk@2gis.ru>
2015-12-21 14:17:55 +0600

    fix bug

--- CustomerIntelligence/CustomerIntelligence.Domain/Specifications/Specs.Map.Facts.Statistics.cs
+++ CustomerIntelligence/CustomerIntelligence.Domain/Specifications/Specs.Map.Facts.Statistics.cs
@@ -22,43 +22,50 @@ namespace NuClear.CustomerIntelligence.Domain.Specifications
                         new MapSpecification<IQuery, IQueryable<Statistics::FirmCategory3>>(
                             q =>
                                 {
-                                    var firmDtos = (from firm in q.For<Facts::Firm>()
+                                    var firmDtos = from firm in q.For<Facts::Firm>()
                                                     join project in q.For<Facts::Project>() on firm.OrganizationUnitId equals project.OrganizationUnitId
                                                     join firmAddress in q.For<Facts::FirmAddress>() on firm.Id equals firmAddress.FirmId
                                                     join categoryFirmAddress in q.For<Facts::CategoryFirmAddress>() on firmAddress.Id equals categoryFirmAddress.FirmAddressId
-                                                    select new { FirmId = firm.Id, ProjectId = project.Id, categoryFirmAddress.CategoryId }).Distinct();
+                                                    select new
+                                                    {
+                                                        FirmId = firm.Id,
+                                                        ProjectId = project.Id,
+                                                        categoryFirmAddress.CategoryId
+                                                    };
 
                                     var firmCounts = from firm in firmDtos
                                                      group firm by new { firm.ProjectId, firm.CategoryId }
                                                      into grp
-                                                     // grp.Key.ProjectId не работает в sqllite
-                                                     // переписать на grp.Key.ProjectId когда будет im-memory join
-                                                     select new { grp.First().ProjectId, grp.First().CategoryId, Count = grp.Count() };
+                                                     select new
+                                                     {
+                                                         grp.Key.ProjectId,
+                                                         grp.Key.CategoryId,
+                                                         Count = grp.Count()
+                                                     };
 
-                                    var categories3 = from firmDto in firmDtos
-                                            join category in q.For<Facts::Category>() on firmDto.CategoryId equals category.Id
-                                            from firmStatistics in q.For<Bit::FirmCategoryStatistics>()
-                                                                .Where(x => x.FirmId == firmDto.FirmId && x.CategoryId == firmDto.CategoryId && x.ProjectId == firmDto.ProjectId)
-                                                                .DefaultIfEmpty()
-                                        from categoryStatistics in q.For<Bit::ProjectCategoryStatistics>()
-                                                                    .Where(x => x.CategoryId == firmDto.CategoryId && x.ProjectId == firmDto.ProjectId)
-                                                                    .DefaultIfEmpty()
-                                        from firmCount in firmCounts.Where(x => x.CategoryId == firmDto.CategoryId && x.ProjectId == firmDto.ProjectId)
-                                                                    .DefaultIfEmpty()
-                                        select new Statistics::FirmCategory3
-                                               {
-                                                   ProjectId = firmDto.ProjectId,
-                                                   FirmId = firmDto.FirmId,
-                                                   CategoryId = firmDto.CategoryId,
-                                                   Name = category.Name,
-                                                   Hits = firmStatistics == null ? 0 : firmStatistics.Hits,
-                                                   Shows = firmStatistics == null ? 0 : firmStatistics.Shows,
-                                                   FirmCount = firmCount.Count,
-                                                   AdvertisersShare = categoryStatistics == null ? 0 : Math.Min(1, (float)categoryStatistics.AdvertisersCount / firmCount.Count)
-                                               };
+                                    var categories3 = from firmDto in firmDtos.Distinct()
+                                                      join firmCount in firmCounts on new { firmDto.ProjectId, firmDto.CategoryId } equals new { firmCount.ProjectId, firmCount.CategoryId }
+                                                      join category in q.For<Facts::Category>() on firmDto.CategoryId equals category.Id
+                                                      from firmStatistics in q.For<Bit::FirmCategoryStatistics>()
+                                                                                  .Where(x => x.FirmId == firmDto.FirmId && x.CategoryId == firmDto.CategoryId && x.ProjectId == firmDto.ProjectId)
+                                                                                  .DefaultIfEmpty()
+                                                      from categoryStatistics in q.For<Bit::ProjectCategoryStatistics>()
+                                                                                  .Where(x => x.CategoryId == firmDto.CategoryId && x.ProjectId == firmDto.ProjectId)
+                                                                                  .DefaultIfEmpty()
+                                                      select new Statistics::FirmCategory3
+                                                      {
+                                                          ProjectId = firmDto.ProjectId,
+                                                          FirmId = firmDto.FirmId,
+                                                          CategoryId = firmDto.CategoryId,
+                                                          Name = category.Name,
+                                                          Hits = firmStatistics == null ? 0 : firmStatistics.Hits,
+                                                          Shows = firmStatistics == null ? 0 : firmStatistics.Shows,
+                                                          FirmCount = firmCount.Count,
+                                                          AdvertisersShare = categoryStatistics == null ? 0 : Math.Min(1, (float)categoryStatistics.AdvertisersCount / firmCount.Count)
+                                                      };
 
-                                return categories3;
-                            });
+                                    return categories3;
+                                });
                 }
             }
         }
